15a226303da52e989a34a9ddbb71680c7c01ffc8
MW Felker <mwadefelker@gmail.com>
2020-02-20 15:09:32 -0600

    First push

---
+++ Assets/TerrainGenerator/Code/Generator/Chunk/TerrainChunkGenerator.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace TerrainGenerator
+{
+    public class TerrainChunkGenerator : MonoBehaviour
+    {
+        public Material TerrainMaterial;
+
+        public Texture2D FlatTexture;
+        public Texture2D SteepTexture;
+
+		public float perlinHeight = 1.5F;
+		public float perlinDamper = 0.25F;
+
+        private TerrainChunkSettings Settings;
+
+        private NoiseProvider NoiseProvider;
+
+        private ChunkCache Cache;
+
+        private void Awake()
+        {
+            Settings = new TerrainChunkSettings(129, 129, 100, 40, FlatTexture, SteepTexture, TerrainMaterial);
+
+            Cache = new ChunkCache();
+        }
+
+		private void RandomizePerlin() {
+			perlinHeight = Random.Range (perlinHeight, 4.0F);
+			perlinDamper = Random.Range (perlinDamper, 2.5F);
+		}
+
+        private void Update()
+        {
+			Cache.Update();
+        }
+
+        private void GenerateChunk(int x, int z)
+        {
+            if (Cache.ChunkCanBeAdded(x, z))
+            {
+				NoiseProvider = new NoiseProvider(perlinHeight, perlinDamper);
+				var chunk = new TerrainChunk(Settings, NoiseProvider, x, z);
+                Cache.AddNewChunk(chunk);
+            }
+        }
+
+        private void RemoveChunk(int x, int z)
+        {
+            if (Cache.ChunkCanBeRemoved(x, z))
+                Cache.RemoveChunk(x, z);
+        }
+
+        private List<Vector2i> GetChunkPositionsInRadius(Vector2i chunkPosition, int radius)
+        {
+            var result = new List<Vector2i>();
+
+            for (var zCircle = -radius; zCircle <= radius; zCircle++)
+            {
+                for (var xCircle = -radius; xCircle <= radius; xCircle++)
+                {
+                    if (xCircle * xCircle + zCircle * zCircle < radius * radius)
+                        result.Add(new Vector2i(chunkPosition.X + xCircle, chunkPosition.Z + zCircle));
+                }
+            }
+
+            return result;
+        }
+
+        public void UpdateTerrain(Vector3 worldPosition, int radius)
+        {
+            var chunkPosition = GetChunkPosition(worldPosition);
+            var newPositions = GetChunkPositionsInRadius(chunkPosition, radius);
+
+            var loadedChunks = Cache.GetGeneratedChunks();
+            var chunksToRemove = loadedChunks.Except(newPositions).ToList();
+
+            var positionsToGenerate = newPositions.Except(chunksToRemove).ToList();
+            foreach (var position in positionsToGenerate)
+                GenerateChunk(position.X, position.Z);
+
+            //foreach (var position in chunksToRemove)
+             //   RemoveChunk(position.X, position.Z);
+        }
+
+        public Vector2i GetChunkPosition(Vector3 worldPosition)
+        {
+            var x = (int)Mathf.Floor(worldPosition.x / Settings.Length);
+            var z = (int)Mathf.Floor(worldPosition.z / Settings.Length);
+
+            return new Vector2i(x, z);
+        }
+
+        public bool IsTerrainAvailable(Vector3 worldPosition)
+        {
+            var chunkPosition = GetChunkPosition(worldPosition);
+            return Cache.IsChunkGenerated(chunkPosition);
+        }
+
+        public float GetTerrainHeight(Vector3 worldPosition)
+        {
+            var chunkPosition = GetChunkPosition(worldPosition);
+            var chunk = Cache.GetGeneratedChunk(chunkPosition);
+            if (chunkPosition != null)
+                return chunk.GetTerrainHeight(worldPosition);
+
+            return 0;
+        }
+    }
+}
---
+++ Assets/TerrainGenerator/Code/Generator/Noise/NoiseProvider.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+using LibNoise.Generator;
+
+namespace TerrainGenerator
+{
+    public class NoiseProvider : INoiseProvider
+    {
+		private float height;
+		private float damper;
+
+        private Perlin PerlinNoiseGenerator;
+
+        public NoiseProvider(float perlinHeight,float perlinDamper)
+        {
+			height = perlinHeight;
+			damper = perlinDamper;
+            PerlinNoiseGenerator = new Perlin();
+        }
+
+        public float GetValue(float x, float z)
+        {
+            return (float)(PerlinNoiseGenerator.GetValue(x, 0, z) / height) + damper;
+        }
+
+    }
+}
