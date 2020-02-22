90de9e628672b62c89b3b259f24e4816eb084644
KdotJPG <kdotjpg@gmail.com>
2020-02-21 21:20:36 -0500

    Rotate 3D Perlin to reduce visible grid alignment

--- Assets/TerrainGenerator/Code/Generator/Noise/NoiseProvider.cs
+++ Assets/TerrainGenerator/Code/Generator/Noise/NoiseProvider.cs
@@ -20,7 +20,21 @@ namespace TerrainGenerator
 
         public float GetValue(float x, float z)
         {
-            return (float)(PerlinNoiseGenerator.GetValue(x, 0, z) / height) + damper;
+            (float xr, float yr, float zr) = rotateXZBeforeY(x, 0, z);
+            return (float)(PerlinNoiseGenerator.GetValue(xr, yr, zr) / height) + damper;
+        }
+
+        // Old Perlin noise can be visibly grid-aligned. Simplex-type noise is usually better about that.
+        // However, in the absence of Simplex, 3D Perlin can be rotated to hide the grid in X/Z planes.
+        private (float xr, float yr, float zr) rotateXZBeforeY(float x, float y, float z)
+        {
+            float xz = x + z;
+            float yy = y * 0.577350269189626f;
+            float s2 = xz * -0.211324865405187f + yy;
+            float xr = x + s2;
+            float zr = z + s2;
+            float yr = xz * -0.577350269189626f + yy;
+            return (xr, yr, zr);
         }
 
     }
